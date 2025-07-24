2923aa54216d481e70a89639e57ff84657bb6bb0
Shubh Bansal <shubhbansal44@gmail.com>
2025-07-24 17:57:33 +0530

    chore: verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -252,7 +252,8 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 
 	// GetProduct will fail on a specific product when feature flag is enabled
 	if p.checkProductFailure(ctx, req.Id) {
-		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag Enabled")
+		msg := "Error: Product Catalog Fail Feature Flag Enabled"
+
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, msg)
@@ -309,12 +310,12 @@ func (p *productCatalog) checkProductFailure(ctx context.Context, id string) boo
 	return failureEnabled
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
+// func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
+// 	return grpc.DialContext(ctx, svcAddr,
+// 		grpc.WithTransportCredentials(insecure.NewCredentials()),
+// 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+// 	)
+// }
 
 
 
